f17eb8734589580f582410e64d5c9efd9c15762e
pedrocleal <pedroxleal10@gmail.com>
2022-05-01 23:31:07 -0300

    Finish home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,71 @@
-import { Container, InputSearchContainer } from './styles';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {
+  Container, InputSearchContainer,
+  HomeHeader, ListContacts, Card,
+} from './styles';
+
+import edit from '../../assets/images/edit.svg';
+import trash from '../../assets/images/trash.svg';
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [contacts, setContacts] = useState([]);
+
+  const filteredContacts = contacts.filter(
+    (contact) => contact.name.toUpperCase().includes(searchTerm.toUpperCase()),
+  );
+
+  useEffect(() => {
+    fetch('http://localhost:3030/contacts')
+      .then(async (response) => {
+        const data = await response.json();
+        setContacts(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <Container>
 
-      <InputSearchContainer placeholder="Pesquisar um contato" />
+      <InputSearchContainer
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Pesquisar um contato..."
+      />
+
+      <HomeHeader>
+        <span>
+          {contacts.length}
+          {' '}
+          {contacts.length !== 1 ? 'contatos' : 'contato'}
+        </span>
+        <Link to="/new">Novo contato</Link>
+      </HomeHeader>
+
+      <hr />
+
+      <ListContacts>
+        {filteredContacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="contact-info">
+              <p>{contact.name}</p>
+              <span>{contact.phone}</span>
+            </div>
+            <div className="actions">
+              <Link to={`edit/${contact.id}`}>
+                <img src={edit} alt="Edit button" />
+              </Link>
+              <Link to={`delete/${contact.id}`}>
+                <img src={trash} alt="Delete button" />
+              </Link>
+            </div>
+          </Card>
+        ))}
+      </ListContacts>
 
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,6 +7,14 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  hr {
+    width: 100%;
+    margin-top: 32px;
+    background: ${({ theme }) => theme.colors.grey};
+    height: 2px;
+    border: none;
+  }
 `;
 
 export const InputSearchContainer = styled.input`
@@ -22,5 +30,66 @@ export const InputSearchContainer = styled.input`
 
   &::placeholder {
     color: #525252;
+  }
+`;
+
+export const HomeHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 52px;
+  width: 100%;
+
+  span {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  /* Novo contato */
+  a {
+    padding: 8px 16px;
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.colors.purple};
+    font-size: 18px;
+    font-weight: bold;
+    border: 2px solid ${({ theme }) => theme.colors.purple};
+    border-radius: 8px;
+  }
+`;
+
+export const ListContacts = styled.div`
+  margin-top: 32px;
+  width: 100%;
+  max-width: 500px;
+`;
+
+export const Card = styled.div`
+  margin-bottom: 24px;
+  padding: 16px;
+  background: ${({ theme }) => theme.colors.grey};
+  border-radius: 8px;
+  box-shadow: ${({ theme }) => theme.styles.boxShadow};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .contact-info {
+    p {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    span {
+      display: block;
+      margin-top: 8px;
+      color: ${({ theme }) => theme.colors.purple};
+      font-weight: bold;
+    }
+  }
+
+  .actions * {
+    margin-left: 4px;
   }
 `;
